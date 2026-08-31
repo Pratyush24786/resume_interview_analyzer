923380fa62cc3c183e1551e8550f8e4081827724
Pratyush24786 <116939234+Pratyush24786@users.noreply.github.com>
2026-08-31 15:15:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
 window.CAREER_MATCH_CONFIG = {
-  personalisedHelpUrl: "mailto:pratyushkhare786@gmail.com?subject=CareerMatch%20personalised%20help",
-  feedbackUrl: "https://mail.google.com/mail/?view=cm&fs=1&to=pratyushkhare786@gmail.com&su=CareerMatch%20feedback",
-  contactUrl: "mailto:pratyushkhare786@gmail.com",
-  analyticsMeasurementId: ""
+  personalisedHelpUrl:
+    "mailto:pratyushkhare786@gmail.com?subject=CareerMatch%20personalised%20help",
+
+  feedbackUrl:
+    "mailto:pratyushkhare786@gmail.com?subject=CareerMatch%20Feedback&body=Hi%20Pratyush%2C%0A%0AI%20used%20CareerMatch%20and%20wanted%20to%20share%20some%20feedback%3A%0A%0A",
+
+  contactUrl:
+    "mailto:pratyushkhare786@gmail.com",
+
+  analyticsMeasurementId: "mailto:pratyushkhare786@gmail.com?subject=CareerMatch%20Feedback&body=Hi%20Pratyush%2C%0A%0AI%20used%20CareerMatch%20and%20wanted%20to%20share%20some%20feedback%3A%0A%0A",
+
 };
