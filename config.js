b8617499699e6049e83b55bd90ba3aed98fba307
Pratyush24786 <116939234+Pratyush24786@users.noreply.github.com>
2026-08-31 15:02:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.CAREER_MATCH_CONFIG = {
   personalisedHelpUrl: "mailto:pratyushkhare786@gmail.com?subject=CareerMatch%20personalised%20help",
   feedbackUrl: "mailto:pratyushkhare786@gmail.com?subject=CareerMatch%20feedback",
-  contactUrl: "mailto:pratyushkhare786@gmail.com"
+  contactUrl: "mailto:pratyushkhare786@gmail.com",
   analyticsMeasurementId: ""
 };
